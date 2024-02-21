ecc4d6ac641f301258d3420badba13d972d9e846
nik0zer <s21b1.nikozer@gmail.com>
2024-02-21 18:05:33 +0300

    +Splay operation

--- splayTree/include/splayTree.h
+++ splayTree/include/splayTree.h
@@ -7,19 +7,83 @@ namespace SplayTree
     class Node : public std::enable_shared_from_this<Node<T>>
     {
         using std::enable_shared_from_this<Node<T>>::shared_from_this;
-        private:
+        public:
             T _data;
             std::shared_ptr<Node<T>> _left_node;
             std::shared_ptr<Node<T>> _right_node;
             std::weak_ptr<Node<T>> _predessor_node;
+            void Zig();
+            void ZigZig();
+            void ZigZag();
+            
         public:
             T GetData();
             Node(T data);
             void TestInsert(T data);
             std::shared_ptr<Node<T>> RightRotate();
             std::shared_ptr<Node<T>> LeftRotate();
+            void EdgeRotate(std::shared_ptr<Node<T>> predessor, std::shared_ptr<Node<T>> rotate_node);
+            void Splay();
             void PrintPreOrderDFS();
     };
+
+    template <typename T> void Node<T>::EdgeRotate(std::shared_ptr<Node<T>> predessor, std::shared_ptr<Node<T>> rotate_node)
+    {
+        if(predessor->_left_node == rotate_node)
+        {
+            predessor->RightRotate();
+        }
+        else
+        {
+            predessor->LeftRotate();
+        }
+    }
+
+    template <typename T> void Node<T>::Splay()
+    {
+        if(_predessor_node.lock() == nullptr)
+        {
+            return;
+        }
+        if(_predessor_node.lock()->_predessor_node.lock() == nullptr)
+        {
+            Zig();
+            return;
+        }
+        if((_predessor_node->_left_node == shared_from_this() &&
+        _predessor_node.lock()->_predessor_node.lock()->_left_node == _predessor_node.lock()) || 
+        (_predessor_node->_right_node == shared_from_this() &&
+        _predessor_node.lock()->_predessor_node.lock()->_right_node == _predessor_node.lock()))
+        {
+            ZigZig();
+        }
+        else
+        {
+            ZigZag();
+        }
+        if(_predessor_node.lock() == nullptr)
+        {
+            return;
+        }
+        Splay();
+    }
+
+    template <typename T> void Node<T>::ZigZig()
+    {
+        EdgeRotate(_predessor_node.lock()->_predessor_node, _predessor_node);
+        EdgeRotate(_predessor_node, shared_from_this());
+    }
+
+    template <typename T> void Node<T>::ZigZag()
+    {
+        EdgeRotate(_predessor_node, shared_from_this());
+        EdgeRotate(_predessor_node, shared_from_this());
+    }
+
+    template <typename T> void Node<T>::Zig()
+    {
+        EdgeRotate(_predessor_node.lock(), shared_from_this());
+    }
 
     template <typename T> void Node<T>::PrintPreOrderDFS()
     {
@@ -39,9 +103,10 @@ namespace SplayTree
         std::shared_ptr<Node<T>> temp = _right_node->_left_node;
         std::shared_ptr<Node<T>> new_root_node = _right_node;
         new_root_node->_left_node = shared_from_this();
-        new_root_node->_predessor_node = new_root_node->_left_node->_predessor_node;
+        new_root_node->_predessor_node = _predessor_node;
         _predessor_node = new_root_node;
         _right_node = temp;
+        temp->_predessor_node = shared_from_this();
         return new_root_node;
     }
 
@@ -54,9 +119,10 @@ namespace SplayTree
         std::shared_ptr<Node<T>> temp = _left_node->_right_node;
         std::shared_ptr<Node<T>> new_root_node = _left_node;
         new_root_node->_right_node = shared_from_this();
-        new_root_node->_predessor_node = new_root_node->_right_node->_predessor_node;
+        new_root_node->_predessor_node = _predessor_node;
         _predessor_node = new_root_node;
         _left_node = temp;
+        temp->_predessor_node = shared_from_this();
         return new_root_node;
     }
 
@@ -75,6 +141,7 @@ namespace SplayTree
                 return;
             }
             _left_node = std::shared_ptr<Node<T>>(new Node<T>(data));
+            _left_node->_predessor_node = shared_from_this();
         }
         else
         {
@@ -84,6 +151,7 @@ namespace SplayTree
                 return;
             }
             _right_node = std::shared_ptr<Node<T>>(new Node<T>(data));
+            _right_node->_predessor_node = shared_from_this();
         }
     }
     template <typename T> T Node<T>::GetData()
